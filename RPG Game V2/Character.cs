565abb011f1348176ced79c791b6409c7c226062
Dragoş <edragos33@gmail.com>
2025-04-12 10:50:31 +0200

    character class created

---
+++ RPG Game V2/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Game_V2
+{
+    internal class Character
+    {
+        private string name;
+        private int strenth;
+        private int intellect;
+
+        public Character(string name, int strenth, int intellect)
+        {   
+            this.name = name;
+            this.strenth = strenth;
+            this.intellect = intellect;
+        }
+
+
+        public override string ToString() 
+        { 
+            return $"Name: {name}" +
+                $"\nStrength: {strenth}" +
+                $"\nIntellect: {intellect}";
+        }
+
+    }
+}
---
+++ RPG Game V2/Program.cs
@@ -0,0 +1,15 @@
+﻿using RPG_Game_V2;
+using System.Diagnostics;
+using System.Threading;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Character sam = new Character("Sam", 20, 5);
+
+        Console.WriteLine(sam);
+
+    }
+}
